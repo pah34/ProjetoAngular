2271a929f1007c819426479795e232a71b1b112d
pah34 <etecpaloma@gmail.com>
2024-09-29 17:47:12 +0000

    gorgeta - :)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule, CommonModule], // Verifique se `FormsModule` está importado!
+  standalone: true
 })
 export class HomePage {
-  constructor() {}
+  public valorConta: number | undefined;
+  public porcentagemGorjeta: number | undefined;
+  public valorGorjeta: number = 0;
+  public totalConta: number = 0;
+
+  calcularGorjeta() {
+    if (this.valorConta && this.porcentagemGorjeta) {
+      this.valorGorjeta = (this.valorConta * this.porcentagemGorjeta) / 100;
+      this.totalConta = this.valorConta + this.valorGorjeta;
+    }
+  }
 }
