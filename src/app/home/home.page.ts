866497f40e287efb23e8d4d8a9d56b191517416c
pah34 <etecpaloma@gmail.com>
2024-09-29 16:41:27 +0000

    °C - F

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +7,15 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  public celsius: number | undefined;
+  public fahrenheit: number | undefined;
+
+  converterTemperatura() {
+    if (this.celsius !== undefined) {
+      this.fahrenheit = (this.celsius * 9/5) + 32;
+    }
+  }
 }
