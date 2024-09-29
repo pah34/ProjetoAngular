2a1a2d2ad3e1e502f824c468e9ad24b2a19d9742
pah34 <etecpaloma@gmail.com>
2024-09-29 17:21:09 +0000

    lista tarefas - :)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule, CommonModule],
+  standalone: true
 })
 export class HomePage {
-  constructor() {}
+  public novaTarefa: string = ""; // Inicializar nova tarefa como uma string vazia
+  public tarefas: Array<{ nome: string; concluida: boolean }> = []; // Inicializa a lista de tarefas vazia
+
+  adicionarTarefa() {
+    if (this.novaTarefa.trim().length > 0) {
+      this.tarefas.push({ nome: this.novaTarefa, concluida: false });
+      console.log(this.tarefas); // Verificar se a tarefa foi adicionada
+      this.novaTarefa = "";
+    }
+  }
+
+  removerTarefa(tarefa: { nome: string; concluida: boolean }) {
+    // Filtrar o array para remover a tarefa correspondente
+    this.tarefas = this.tarefas.filter(t => t !== tarefa);
+  }
+  
 }
