d468454034d67019c5cbe540d51816c935d6b8df
pah34 <etecpaloma@gmail.com>
2024-09-29 17:08:49 +0000

    R$ - USD - :)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +8,20 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule, HttpClientModule],
 })
 export class HomePage {
-  constructor() {}
+  public valorBRL: number | undefined;
+  public valorUSD: number | undefined;
+
+  constructor(private http: HttpClient) {}
+
+  converterMoeda() {
+    if (this.valorBRL) {
+      this.http.get<any>('https://api.exchangerate-api.com/v4/latest/BRL').subscribe(data => {
+        const taxa = data.rates.USD;
+        this.valorUSD = parseFloat((this.valorBRL! * taxa).toFixed(2));
+      });
+    }
+  }
 }
