9d00e72a9db1fea76a09907cb0de4aeda46b0eb9
pah34 <etecpaloma@gmail.com>
2024-09-29 16:27:05 +0000

    imc - :)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +7,26 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  public peso: number | undefined;
+  public altura: number | undefined;
+  public resultado: number | undefined;
+  public classificacao: string = "";
+
+  calcularIMC() {
+    if (this.peso && this.altura) {
+      this.resultado = this.peso / (this.altura * this.altura);
+      if (this.resultado < 18.5) {
+        this.classificacao = "Abaixo do peso";
+      } else if (this.resultado >= 18.5 && this.resultado <= 24.9) {
+        this.classificacao = "Peso normal";
+      } else if (this.resultado >= 25 && this.resultado <= 29.9) {
+        this.classificacao = "Sobrepeso";
+      } else {
+        this.classificacao = "Obesidade";
+      }
+    }
+  }
 }
