60d2115b0bfc72e8567ce976de4add9265f002f8
pah34 <etecpaloma@gmail.com>
2024-09-29 17:32:34 +0000

    calculadora - :)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +7,29 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  public numero1: number | undefined;
+  public numero2: number | undefined;
+  public resultado: number | undefined;
+
+  calcular(operacao: string) {
+    if (this.numero1 !== undefined && this.numero2 !== undefined) {
+      switch (operacao) {
+        case 'somar':
+          this.resultado = this.numero1 + this.numero2;
+          break;
+        case 'subtrair':
+          this.resultado = this.numero1 - this.numero2;
+          break;
+        case 'multiplicar':
+          this.resultado = this.numero1 * this.numero2;
+          break;
+        case 'dividir':
+          this.resultado = this.numero1 / this.numero2;
+          break;
+      }
+    }
+  }
 }
