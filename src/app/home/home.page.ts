1aafa1e46b24d63674fc95566fa751a5803f1b87
pah34 <etecpaloma@gmail.com>
2024-09-29 22:19:55 +0000

    lista musica - :)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +8,31 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  public nome = "";
+  public items: any[] = [
+    {
+      titulo: "Forró"
+    },
+    {
+      titulo: "Rock"
+    },
+    {
+      titulo: "Sertanejo"
+    },
+    {
+      titulo: "Funk"
+    }
+  ]
+
+  adicionar() {
+    this.items.push({ titulo: this.nome });
+    this.nome = "";
+  }
+
+  remover(posicao: number) {
+    this.items.splice(posicao, 1)
+  }
 }
