bc6010e8eee8128b76e8013ebb429e0c7a50c936
pah34 <etecpaloma@gmail.com>
2024-09-29 18:40:03 +0000

    gasolina - alcool

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  public gasolina: number | undefined;
+  public alcool: number | undefined;
+  public razao: number | undefined;
+  public situacao: string = "";
+  constructor(private alertController: AlertController) { }
+
+  calcularBene() {
+    if (this.gasolina && this.alcool) {
+      this.razao = this.alcool / this.gasolina;
+      if (this.razao <= 0.7) {
+        this.situacao = "Álcool compensa";
+      }
+      else if (this.razao > 0.7) {
+        this.situacao = "Gasolina compensa";
+      }
+      else {
+        this.exibirAlerta();
+      }
+    }
+  }
+
+  async exibirAlerta() {
+    const alert = await this.alertController.create({
+      header: 'Atenção',
+      message: 'Por favor, digite os doas valores antes de fazer o cálculo',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 }
