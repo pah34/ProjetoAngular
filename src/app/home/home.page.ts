611052e5f94c73f258c4b17591c9b02ccd0214e3
pah34 <etecpaloma@gmail.com>
2024-09-29 18:11:30 +0000

    juros simples - :)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule, CommonModule],
 })
 export class HomePage {
   constructor() {}
+  public capital: number | undefined;
+  public taxaJuros: number | undefined;
+  public tempo: number | undefined;
+  public montante: number | undefined;
+
+  calcularMontante() {
+    if (this.capital && this.taxaJuros && this.tempo) {
+      this.montante = this.capital * (1 + (this.taxaJuros / 100) * this.tempo);
+    }
+  }
 }
