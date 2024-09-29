607582a5cf90d362a2943bd8964dbd494b38e023
pah34 <etecpaloma@gmail.com>
2024-09-29 17:57:54 +0000

    milhas - :)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +7,16 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
   constructor() {}
+  public distanciaKm: number | undefined;
+  public resultadoMilhas: number | undefined;
+
+  converterDistancia() {
+    if (this.distanciaKm !== undefined) {
+      this.resultadoMilhas = this.distanciaKm * 0.621371;
+    }
+  }
 }
