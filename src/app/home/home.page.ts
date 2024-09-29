43a080175ff1cf6d4db53319896103e979d60e5f
pah34 <etecpaloma@gmail.com>
2024-09-29 22:37:50 +0000

    calculo de média

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +7,33 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
+  notaA: number | null = null;
+  notaB: number | null = null;
+  media: number | null = null;
+  situacao: string = '';
+
   constructor() {}
+
+  calcularMedia() {
+    if (this.notaA !== null && this.notaB !== null) {
+      // Calcula a média das notas
+      this.media = (this.notaA + this.notaB) / 2;
+
+      // Define a situação com base na média
+      if (this.media >= 7) {
+        this.situacao = 'Aprovado';
+      } else if (this.media >= 5) {
+        this.situacao = 'Recuperação';
+      } else {
+        this.situacao = 'Reprovado';
+      }
+    } else {
+      // Caso as notas não estejam preenchidas
+      this.situacao = 'Insira as notas A e B';
+      this.media = null;
+    }
+  }
 }
